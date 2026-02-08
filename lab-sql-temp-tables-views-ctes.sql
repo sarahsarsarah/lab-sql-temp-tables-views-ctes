70ebde71ddcfc3966f8f3f36bfeba6f03bdff8ce
sarahsarsarah <aljamalsara1@gmail.com>
2026-02-09 00:19:31 +0100

    Solved lab

---
+++ lab-sql-temp-tables-views-ctes.sql
@@ -0,0 +1,45 @@
+CREATE VIEW customer_rental_summary AS
+SELECT 
+    c.customer_id,
+    CONCAT(c.first_name, ' ', c.last_name) AS customer_name,
+    c.email,
+    COUNT(r.rental_id) AS rental_count
+FROM 
+    sakila.customer c
+LEFT JOIN 
+    sakila.rental r ON c.customer_id = r.customer_id
+GROUP BY 
+    c.customer_id, c.first_name, c.last_name, c.email;
+    
+    CREATE TEMPORARY TABLE customer_payment_summary AS
+SELECT 
+    crs.customer_id,
+    SUM(p.amount) AS total_paid
+FROM 
+    customer_rental_summary crs
+LEFT JOIN 
+    sakila.payment p ON crs.customer_id = p.customer_id
+GROUP BY 
+    crs.customer_id;
+    
+    WITH customer_summary AS (
+    SELECT 
+        crs.customer_name,
+        crs.email,
+        crs.rental_count,
+        cps.total_paid,
+        ROUND(cps.total_paid / crs.rental_count, 2) AS average_payment_per_rental
+    FROM 
+        customer_rental_summary crs
+    LEFT JOIN 
+        customer_payment_summary cps ON crs.customer_id = cps.customer_id
+)
+
+SELECT 
+    customer_name, 
+    email, 
+    rental_count, 
+    total_paid, 
+    average_payment_per_rental
+FROM 
+    customer_summary;
